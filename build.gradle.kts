98d6fdd465a32648d9089ef1045f35aa142abdda
taroq4-2 <taroq4@gmail.com>
2026-06-20 16:57:32 +0300

    Add build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,18 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.1.4")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
