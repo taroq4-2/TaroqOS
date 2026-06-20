31f4c91fe10f167bbc0d9aa05c8231ee7322b7f1
taroq4-2 <taroq4@gmail.com>
2026-06-20 17:37:02 +0300

    fix: build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,4 @@
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
-    }
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.1.4")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
-    }
-}
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
+plugins {
+    id("com.android.application") version "8.1.4" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
 }
